3ea71d842a077a4c1e962268dd9e025865ce25ab
Elia Hawley <elhawley@ucsc.edu>
2023-06-10 20:51:18 -0700

    editing airstripplay

--- src/scenes/AirstripPlay.js
+++ src/scenes/AirstripPlay.js
@@ -16,27 +16,41 @@ class AirstripPlay extends Phaser.Scene {
         this.load.spritesheet('airstrip', './assets/airstripbackground.png');
         this.load.image('cop', './assets/Cop.js');
         this.load.image('robber', './assets/Robber.js');
-        this.load.image('box1', './assets/box1.png')
+        this.load.image('box1', './assets/box1.png');
+        this.load.image('airstripImage', 'city-tileset.png');
+        this.load.tilemapTiledJSON('airstripJSON', 'airstrip-tilemap.json');
     }
 
     create() {
-        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
-        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
-        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
-        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
-        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+        const map = this.add.tilemap('airstripJSON');
+        const tileset = map.addTilesetImage('tileset', 'airstripImage');
+        const bgLayer = map.createLayer('Background', tileset, 0, 0);
+        const terrainLayer = map.createLayer('Terrain', tileset, 0, 0);
+        const boxLayer = map.createLayer('Boxes', tileset, 0, 0);
+
+        this.cop = this.physics.add.sprite(50, 50, 'cop', 0);
+        this.robber = this.physics.add.sprite(700, 700, 'robber', 0);
 
         this.airstrip = this.add.tileSprite(0, 0, 800, 800, 'airstrip').setOrigin(0, 0);
 
-        this.cop = new Cop(this, borderUISize + borderPadding, game.config.height - borderUISize - borderPadding - (30*1.5), 'cop').setOrigin(1, 0);
-        this.robber = new Robber(this, borderUISize + borderPadding, game.config.height - borderUISize - borderPadding - (30*1.5), 'robber').setOrigin(700, 700);
 
-    
+
         // logic
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.cop, true, 0.25, 0.25);
+
         this.gameOver = false;
+
+        terrainLayer.setCollisionByProperty({ collides: true });
+        this.physics.add.collider(this.slime, terrainLayer)
+        treeLayer.setCollisionByProperty({ collides: true });
+        this.physics.add.collider(this.slime, treeLayer)
     }
 
     update() {
+
         if (this.gameOver) {
             this.add.text(game.config.width / 2, game.config.height / 2, 'GAME OVER', this.textConfig).setOrigin(.5);
             this.add.text(game.config.width / 2, game.config.height / 2 + 64, 'Press (R) to Restart', this.textConfig).setOrigin(.5);
@@ -51,17 +65,18 @@ class AirstripPlay extends Phaser.Scene {
         }
 
         if (this.checkCollision(cop, obstacles)) {
-                for (const obstacle of obstacles) {
-                    if (obstacle.visible) {
-                        if (cop.x < obstacle.x + obstacle.displayWidth &&
-                            cop.x + cop.displayWidth > obstacle.x &&
-                            cop.y < obstacle.y + obstacle.displayHeight && 
-                            cop.displayHeight + cop.y > obstacle.y) {
-                                this.gameOver = true;
-                        }
+            for (const obstacle of obstacles) {
+                if (obstacle.visible) {
+                    if (cop.x < obstacle.x + obstacle.displayWidth &&
+                        cop.x + cop.displayWidth > obstacle.x &&
+                        cop.y < obstacle.y + obstacle.displayHeight && 
+                        cop.displayHeight + cop.y > obstacle.y) {
+                            this.gameOver = true;
                     }
                 }
+            }
         }
+
 
         // if (keyLEFT.isDown && this.x >= borderUISize + this.width){ // && this.checkCollision(this, )) { missing collision check between characters & box
         //     this.x -= this.moveSpeed;
