59d4cdf1af0eed3d4385528dc1b82919587d0b3f
elihawley <104234916+elihawley@users.noreply.github.com>
2023-06-12 19:44:12 -0700

    Restaurant scene

--- src/scenes/RestaurantInstructions.js
+++ src/scenes/RestaurantInstructions.js
@@ -7,22 +7,9 @@ class RestaurantInstructions extends Phaser.Scene {
         // load assets
         this.load.path = "./assets/";
 
-        // load JSON (dialog)
-        this.load.json('dialog', 'json/restaurant-dialog.json');
-
-        // load images
-        this.load.image('dialogbox', 'img/dialogbox.png');
-        this.load.image('restaurant_background', 'img/restaurant-background.png');
-        this.load.image('cop_front', 'img/cop-front.png');
-        this.load.image('cop_side', 'img/cop-side.png');
-        this.load.image('cop_smile', 'img/cop-smile.png');
-        this.load.image('robber_front', 'img/robber-front.png');
-        this.load.image('robber_side', 'img/robber-side.png');
-        this.load.image('robber_smile', 'img/robber-smile.png');
-
         // load sounds
-        this.load.audio('bgm_calamity', 'bgm/Calamity.wav');
         this.load.audio('sfx_select', 'sfx/select.wav');
+        this.load.audio('bgm_calamity', 'bgm/Calamity.wav');
         this.load.audio('sfx_type', 'sfx/type.wav');
         this.load.audio('sfx_hit', 'sfx/hit.wav');
         this.load.audio('sfx_gameover', 'sfx/gameover.wav');
@@ -32,15 +19,13 @@ class RestaurantInstructions extends Phaser.Scene {
     }
 
     create() {
-        this.bgm = this.sound.add("bgm_calamity", {volume: 1, loop: true});
-        // this.bgm.play();
-
         // add title text
-        this.add.bitmapText(centerX, centerY - 64, 'gem_font', 'COFFEE SCENE', 32).setOrigin(0.5);
-        this.add.bitmapText(centerX, centerY - 32, 'gem_font', 'Make the right choice', 32).setOrigin(0.5);
-        this.add.bitmapText(centerX, centerY, 'gem_font', 'Use arrow keys and SPACE to select dialog options', 16).setOrigin(0.5);
-        this.add.bitmapText(centerX, centerY + 32, 'gem_font', 'Press SPACE to start', 16).setOrigin(0.5);
-        this.add.bitmapText(centerX, centerY + 64, 'gem_font', 'Press LEFT to return', 16).setOrigin(0.5);
+        this.add.bitmapText(CENTER_X, CENTER_Y - 64, 'gem_font', 'RESTAURANT SCENE', 32).setOrigin(0.5);
+        this.add.bitmapText(CENTER_X, CENTER_Y - 32, 'gem_font', 'A famous conversation between rivals!', 32).setOrigin(0.5);
+        this.add.bitmapText(CENTER_X, CENTER_Y + 32, 'gem_font', 'Use SPACE to continue dialog', 16).setOrigin(0.5);
+        this.add.bitmapText(CENTER_X, CENTER_Y + 64, 'gem_font', 'Hold ESC to leave game', 16).setOrigin(0.5);
+        this.add.bitmapText(CENTER_X, CENTER_Y + 96, 'gem_font', 'Press SPACE to start', 16).setOrigin(0.5);
+        this.add.bitmapText(CENTER_X, CENTER_Y + 128, 'gem_font', 'Press LEFT to return', 16).setOrigin(0.5);
 
         // create input
         cursors = this.input.keyboard.createCursorKeys();
@@ -53,7 +38,6 @@ class RestaurantInstructions extends Phaser.Scene {
             this.scene.start("restaurantPlayScene");
         }
         else if(Phaser.Input.Keyboard.JustDown(cursors.left)) {
-            // this.bgm.stop();
             this.sound.play('sfx_select');
             this.scene.start("menuScene");
         }
--- src/scenes/RestaurantPlay.js
+++ src/scenes/RestaurantPlay.js
@@ -9,40 +9,45 @@ class RestaurantPlay extends Phaser.Scene {
 
         // dialog constants
         this.DBOX_X = 0;			    // dialog box x-position
-        this.DBOX_Y = 400;			    // dialog box y-position
+        this.DBOX_Y = CENTER_Y + 50;			    // dialog box y-position
         this.DBOX_FONT = 'gem_font';	// dialog box font key
 
-        this.TEXT_X = 50;			// text w/in dialog box x-position
-        this.TEXT_Y = 445;			// text w/in dialog box y-position
+        this.TEXT_X = this.DBOX_X + 50;			// text w/in dialog box x-position
+        this.TEXT_Y = this.DBOX_Y + 50;			// text w/in dialog box y-position
         this.TEXT_SIZE = 24;		// text font size (in pixels)
         this.TEXT_MAX_WIDTH = 715;	// max width of text within box
 
         this.NEXT_TEXT = '[SPACE]';	// text to display for next prompt
-        this.NEXT_X = 775;			// next text prompt x-position
-        this.NEXT_Y = 574;			// next text prompt y-position
+        this.NEXT_X = W - 45;			// next text prompt x-position
+        this.NEXT_Y = H - 30;			// next text prompt y-position
 
-        this.LETTER_TIMER = 10;		// # ms each letter takes to "type" onscreen
+        this.LETTER_TIMER = 20;		// # ms each letter takes to "type" onscreen
 
-        // dialog variables
-        this.dialogConvo = 0;			// current "conversation"
-        this.dialogLine = 0;			// current line of conversation
-        this.dialogSpeaker = null;		// current speaker
-        this.dialogLastSpeaker = null;	// last speaker
-        this.dialogTyping = false;		// flag to lock player input while text is "typing"
-        this.dialogText = null;			// the actual dialog text
-        this.nextText = null;			// player prompt text to continue typing
+        this.tweenDuration = 100;
 
-        // character variables
-        this.cop_front = null;
-        this.cop_side = null;
-        this.cop_smile = null;
-        this.robber_front = null;
-        this.robber_side = null;
-        this.robber_smile = null;
-        this.tweenDuration = 200;
+        this.COP_OFFSCREEN_X = -500;        // x,y values to place characters offscreen
+        this.ROBBER_OFFSCREEN_X = W + 500;        // x,y values to place characters offscreen
+        this.OFFSCREEN_Y = CENTER_Y + 25;
+        this.COP_INSCREEN_X = 50;
+        this.ROBBER_INSCREEN_X = W - 256 - 50;
+    }
 
-        this.OFFSCREEN_X = -500;        // x,y values to place characters offscreen
-        this.OFFSCREEN_Y = 1000;
+    preload() {
+        // load assets
+        this.load.path = "./assets/";
+
+        // load JSON (dialog)
+        this.load.json('dialog', 'json/restaurant-dialog.json');
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png');
+        this.load.image('restaurant_background', 'img/restaurant-background.png');
+        this.load.image('cop_front', 'img/cop-front.png');
+        this.load.image('cop_side', 'img/cop-side.png');
+        this.load.image('cop_smile', 'img/cop-smile.png');
+        this.load.image('robber_front', 'img/robber-front.png');
+        this.load.image('robber_side', 'img/robber-side.png');
+        this.load.image('robber_smile', 'img/robber-smile.png');
     }
 
     create() {
@@ -50,10 +55,8 @@ class RestaurantPlay extends Phaser.Scene {
         this.bgm = this.sound.add("bgm_calamity", {volume: 1, loop: true});
         this.bgm.play();
 
-
         // Background Image
-        this.background = this.add.tileSprite(0, 0, 400, 400, 'restaurant-background').setOrigin(0, 0);
-
+        this.background = this.add.tileSprite(0, 0, W, H, 'restaurant_background').setOrigin(0, 0);
 
         // parse dialog from JSON file
         this.dialog = this.cache.json.get('dialog');
@@ -66,129 +69,187 @@ class RestaurantPlay extends Phaser.Scene {
         this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, '', this.TEXT_SIZE);
 
         // ready the character dialog images offscreen
-        this.cop_front = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'cop-front').setOrigin(0, 1);;
-        this.cop_side = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'cop-side').setOrigin(0, 1);;
-        this.cop_smile = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'cop-smile').setOrigin(0, 1);;
-        this.robber_front = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'robber-front').setOrigin(0, 1);;
-        this.robber_side = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'robber-side').setOrigin(0, 1);;
-        this.robber_smile = this.add.sprite(this.OFFSCREEN_X, this.DBOX_Y+8, 'robber-smile').setOrigin(0, 1);;
+        this.cop_front = this.add.sprite(this.COP_OFFSCREEN_X, this.OFFSCREEN_Y, 'cop_front').setOrigin(0, 1);
+        this.cop_side = this.add.sprite(this.COP_OFFSCREEN_X, this.OFFSCREEN_Y, 'cop_side').setOrigin(0, 1);
+        this.cop_smile = this.add.sprite(this.COP_OFFSCREEN_X, this.OFFSCREEN_Y, 'cop_smile').setOrigin(0, 1);
+        this.robber_front = this.add.sprite(this.ROBBER_OFFSCREEN_X, this.OFFSCREEN_Y, 'robber_front').setOrigin(0, 1);
+        this.robber_side = this.add.sprite(this.ROBBER_OFFSCREEN_X, this.OFFSCREEN_Y, 'robber_side').setOrigin(0, 1);
+        this.robber_smile = this.add.sprite(this.ROBBER_OFFSCREEN_X, this.OFFSCREEN_Y, 'robber_smile').setOrigin(0, 1);
 
         // input
         cursors = this.input.keyboard.createCursorKeys();
+        keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC)
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+
+        // logic
+        this.gameOver = false;
+
+        // dialog variables
+        this.dialogConvo = 0;			// current "conversation"
+        this.dialogLine = 0;			// current line of conversation
+        this.dialogSpeaker = null;		// current speaker
+        this.dialogLastSpeaker = null;	// last speaker
+        this.dialogTyping = false;		// flag while text is "typing"
 
         // start dialog
-        this.typeText();
+        this.nextLine();
     }
 
     update() {
+        if (this.gameOver) {
+            this.sound.play('sfx_gameover', {volume: .5});
+            this.add.bitmapText(CENTER_X, CENTER_Y - 32, this.DBOX_FONT, 'GAME OVER', this.TEXT_SIZE).setOrigin(.5);
+            this.add.bitmapText(CENTER_X, CENTER_Y, this.DBOX_FONT, 'Press (R) to Restart or <- for Menu', this.TEXT_SIZE).setOrigin(.5);
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(cursors.left)) {
+            this.sound.stopAll();
+            this.scene.start("menuScene");
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.sound.stopAll();
+            this.scene.restart();
+        }
+
+        if (keyESC.isDown && !Phaser.Input.Keyboard.DownDuration(keyESC, 1500)) {
+            this.sound.stopAll();
+            this.scene.start("menuScene");
+        }
+
         // check for spacebar press
-        if(Phaser.Input.Keyboard.JustDown(cursors.space) && !this.dialogTyping) {
-            // trigger dialog
-            this.typeText();
+        if (Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            if(!this.gameOver && !this.dialogTyping) {
+                // reached end of convo 
+                if(this.dialogLine === this.dialog[this.dialogConvo].length) {
+                    // reached end of last convo
+                    if (this.dialogConvo === this.dialog.length - 1) {
+                        this.gameOver = true;
+                    }
+                    // move to next convo
+                    this.dialogLine = 0;
+                    this.dialogConvo++;
+                }
+    
+                if (!this.gameOver) {
+                    this.nextLine();
+                }
+            }
+            else if (!this.gameOver && this.dialogTyping) {
+                // quick finish line
+                this.dialogTyping = false;
+                this.finishLine();
+            }
         }
     }
 
-    typeText() {
-        // lock input while typing
+    nextLine() {
         this.dialogTyping = true;
 
         // clear text
         this.dialogText.text = '';
         this.nextText.text = '';
-
-        /* Note: In my conversation data structure: 
-                - each array within the main JSON array is a "conversation"
-                - each object within a "conversation" is a "line"
-                - each "line" can have 3 properties: 
-                    1. a speaker (required)
-                    2. the dialog text (required)
-                    3. an (optional) flag indicating if this speaker is new
-        */
-
-        // this.sound.play('sfx_gameover');
-
-        // make sure there are lines left to read in this convo, otherwise jump to next convo
-        if(this.dialogLine > this.dialog[this.dialogConvo].length - 1) {
-            this.dialogLine = 0;
-            // I increment conversations here, but you could create logic to exit the dialog here
-            this.dialogConvo++;
-        }
         
-        // make sure we haven't run out of conversations...
-        if(this.dialogConvo >= this.dialog.length) {
-            // here I'm simply "exiting" the last speaker and removing the dialog box,
-            // but you could build other logic to change game states here
-            console.log('End of Conversations');
+        // set current speaker
+        this.dialogSpeaker = this.dialog[this.dialogConvo][this.dialogLine]['speaker'];
+
+        // check if there's a new speaker (for exit/enter animations)
+        if(this.dialog[this.dialogConvo][this.dialogLine]['newSpeaker']) {
             // tween out prior speaker's image
-            if(this.dialogLastSpeaker) {
+            if(this.dialogLastSpeaker && this.dialogLastSpeaker.startsWith('cop_')) {
                 this.tweens.add({
                     targets: this[this.dialogLastSpeaker],
-                    x: this.OFFSCREEN_X,
+                    x: this.COP_OFFSCREEN_X,
                     duration: this.tweenDuration,
                     ease: 'Linear'
                 });
             }
-            // make text box invisible
-            this.dialogbox.visible = false;
-
-        } else {
-            // if not, set current speaker
-            this.dialogSpeaker = this.dialog[this.dialogConvo][this.dialogLine]['speaker'];
-            // check if there's a new speaker (for exit/enter animations)
-            if(this.dialog[this.dialogConvo][this.dialogLine]['newSpeaker']) {
-                // tween out prior speaker's image
-                if(this.dialogLastSpeaker) {
-                    this.tweens.add({
-                        targets: this[this.dialogLastSpeaker],
-                        x: this.OFFSCREEN_X,
-                        duration: this.tweenDuration,
-                        ease: 'Linear'
-                    });
-                }
-                // tween in new speaker's image
+            else if(this.dialogLastSpeaker && this.dialogLastSpeaker.startsWith('robber_')) {
+                this.tweens.add({
+                    targets: this[this.dialogLastSpeaker],
+                    x: this.ROBBER_OFFSCREEN_X,
+                    duration: this.tweenDuration,
+                    ease: 'Linear'
+                });
+            }
+            // tween in new speaker's image
+            if(this.dialogSpeaker.startsWith('cop_')) {
                 this.tweens.add({
                     targets: this[this.dialogSpeaker],
-                    x: this.DBOX_X + 50,
+                    x: this.COP_INSCREEN_X,
                     duration: this.tweenDuration,
                     ease: 'Linear'
                 });
             }
-
-            // build dialog (concatenate speaker + line of text)
-            this.dialogLines = this.dialog[this.dialogConvo][this.dialogLine]['speaker'].toUpperCase() + ': ' + this.dialog[this.dialogConvo][this.dialogLine]['dialog'];
-
-            // create a timer to iterate through each letter in the dialog text
-            let currentChar = 0; 
-            this.textTimer = this.time.addEvent({
-                delay: this.LETTER_TIMER,
-                repeat: this.dialogLines.length - 1,
-                callback: () => { 
-                    // concatenate next letter from dialogLines
-                    this.dialogText.text += this.dialogLines[currentChar];
-                    // advance character position
-                    currentChar++;
-                    // check if timer has exhausted its repeats 
-                    // (necessary since Phaser 3 no longer seems to have an onComplete event)
-                    if(this.textTimer.getRepeatCount() == 0) {
-                        // show prompt for more text
-                        this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, this.NEXT_TEXT, this.TEXT_SIZE).setOrigin(1);
-                        // un-lock input
-                        this.dialogTyping = false;
-                        // destroy timer
-                        this.textTimer.destroy();
-                    }
-                },
-                callbackScope: this // keep Scene context
-            });
-            
-            // set bounds on dialog
-            this.dialogText.maxWidth = this.TEXT_MAX_WIDTH;
-
-            // increment dialog line
-            this.dialogLine++;
-
-            // set past speaker
-            this.dialogLastSpeaker = this.dialogSpeaker;
+            else if(this.dialogSpeaker.startsWith('robber_')) {
+                this.tweens.add({
+                    targets: this[this.dialogSpeaker],
+                    x: this.ROBBER_INSCREEN_X,
+                    duration: this.tweenDuration,
+                    ease: 'Linear'
+                });
+            }
         }
+        else {
+            // if no "NewSpeaker", then change speaker without tweening
+            if(this.dialogLastSpeaker && this.dialogLastSpeaker.startsWith('cop_')) {
+                this[this.dialogLastSpeaker].x = this.COP_OFFSCREEN_X;
+            }
+            else if(this.dialogLastSpeaker && this.dialogLastSpeaker.startsWith('robber_')) {
+                this[this.dialogLastSpeaker].x = this.ROBBER_OFFSCREEN_X;
+            }
+            // switch in new speaker's image
+            if(this.dialogSpeaker.startsWith('cop_')) {
+                this[this.dialogSpeaker].x = this.COP_INSCREEN_X;
+            }
+            else if(this.dialogSpeaker.startsWith('robber_')) {
+                this[this.dialogSpeaker].x = this.ROBBER_INSCREEN_X;
+            }
+        }
+
+        // build dialog (concatenate speaker + line of text)
+        const speakerName = this.dialog[this.dialogConvo][this.dialogLine]['speaker'].startsWith('cop_') ? 'Cop' : 'Robber';
+        this.dialogLines = speakerName + ': ' + this.dialog[this.dialogConvo][this.dialogLine]['dialog'];
+
+        // create a timer to iterate through each letter in the dialog text
+        let currentChar = 0; 
+        this.textTimer = this.time.addEvent({
+            delay: this.LETTER_TIMER,
+            repeat: this.dialogLines.length - 1,
+            callback: () => { 
+                // concatenate next letter from dialogLines
+                this.dialogText.text += this.dialogLines[currentChar];
+                // advance character position
+                currentChar++;
+                // check if timer has exhausted its repeats 
+                // (necessary since Phaser 3 no longer seems to have an onComplete event)
+                if(this.textTimer.getRepeatCount() == 0) {
+                    // show prompt for more text
+                    this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, this.NEXT_TEXT, this.TEXT_SIZE).setOrigin(1);
+                    // un-lock input
+                    this.dialogTyping = false;
+                    // destroy timer
+                    this.textTimer.destroy();
+                }
+            },
+            callbackScope: this // keep Scene context
+        });
+        
+        // set bounds on dialog
+        this.dialogText.maxWidth = this.TEXT_MAX_WIDTH;
+
+        // increment dialog line
+        this.dialogLine++;
+
+        // set past speaker
+        this.dialogLastSpeaker = this.dialogSpeaker;
+    
+    }
+
+    finishLine() {
+        if (this.textTimer) this.textTimer.destroy();
+
+        const speakerName = this.dialog[this.dialogConvo][this.dialogLine]['speaker'].startsWith('cop_') ? 'Cop' : 'Robber';
+        this.dialogText.text = speakerName + ': ' + this.dialog[this.dialogConvo][this.dialogLine]['dialog'];
+
+        this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, this.NEXT_TEXT, this.TEXT_SIZE).setOrigin(1);
     }
 }
